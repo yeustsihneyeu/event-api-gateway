a6229b62ecf285de726fe87e1015f2ffa6e3f165
ayeustsihneyeu <ayeustsihneyeu@piesoft.us>
2024-03-28 22:02:56 +0100

    first commit

---
+++ src/test/java/com/example/eventapigateway/EventApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
